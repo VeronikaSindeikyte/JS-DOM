14f2f9c4483ba132b138279e9639c1a0387109b8
VeronikaSindeikyte <veronika.sindeikyte@gmail.com>
2024-12-11 14:47:18 +0200

    fixed formating

--- js-finansai/scripts.js
+++ js-finansai/scripts.js
@@ -1,11 +1,16 @@
 let payments = [
-]
+];
 
 function load_data(data_wrapper_selector, data_array, balance_wrapper_selector) {
-    let data_wrapper = document.querySelector(data_wrapper_selector)
-    let balance_wrapper = document.querySelector(balance_wrapper_selector)
+    let data_wrapper = document.querySelector(data_wrapper_selector);
+    let balance_wrapper = document.querySelector(balance_wrapper_selector);
+    let monthlySalary = document.getElementById("salary").valueAsNumber;
 
-    for(const item of data_array) {
+    if (!monthlySalary) {
+        monthlySalary = 0;
+    }
+
+    for (const item of data_array) {
         data_wrapper.innerHTML += `
             <div class="item-wrapper">
                 <p class="id">ID: ${item.id}</p>
@@ -15,16 +20,7 @@ function load_data(data_wrapper_selector, data_array, balance_wrapper_selector) 
                 <p class="amount ${item.type}">${item.amount} €</p>
                 <p class="when">${item.when}</p>
             </div>
-        `
-    }
-
-    let monthlySalary = document.getElementById("salary").valueAsNumber;
-    console.log("Mėnesio atlyginimas:", monthlySalary);
-    console.log("Mėnesio atlyginimas:", typeof monthlySalary);
-    console.log("Mėnesio atlyginimas:", !monthlySalary);
-
-    if (!monthlySalary) {
-        monthlySalary = 0;
+        `;
     }
 
     balance_wrapper.innerHTML = `
@@ -37,39 +33,39 @@ function load_data(data_wrapper_selector, data_array, balance_wrapper_selector) 
                 ${calculateBalance(data_array)} €
             </span>
         <h2>Atlyginimo likutis: ${monthlySalary + calculateBalance(data_array)} €</h2>
-    `
+    `;
 }
 
-load_data('.payments-wrapper', payments, '.balance-wrapper')
+load_data('.payments-wrapper', payments, '.balance-wrapper');
 
 function calculateBalance(payment_data) {
-    let sum = 0
-    for(const item of payment_data) {
-        sum += item.amount
+    let sum = 0;
+    for (const item of payment_data) {
+        sum += item.amount;
     }
-    return sum
+    return sum;
 }
 
 function calculateIncome(payment_data) {
-    let sum = 0
-    for(const item of payment_data) {
-        if(item.type == 'income') {
-            sum += item.amount
+    let sum = 0;
+    for (const item of payment_data) {
+        if (item.type == 'income') {
+            sum += item.amount;
         }
     }
-    return sum
+    return sum;
 }
-
 
 function calculateExpense(payment_data) {
-    return payment_data.filter(item => item.type == 'expense').reduce((sum, item) => sum + item.amount,0)
+    return payment_data.filter(item => item.type == 'expense').reduce((sum, item) => sum + item.amount, 0);
 }
 
-document.getElementById('payment-form').addEventListener('submit', function(event){
-    event.preventDefault()
-    const amount = parseFloat(document.getElementById('amount').value)
-    const type = amount >= 0 ? 'income' : 'expense'
+document.getElementById('payment-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const amount = parseFloat(document.getElementById('amount').value);
+    const type = amount >= 0 ? 'income' : 'expense';
     let piniguSaltinioPavadinimas = document.getElementById("pavadinimas").value;
+
     let resetButton = document.querySelector('.reset');
     resetButton.classList.add('show');
 
@@ -81,40 +77,39 @@ document.getElementById('payment-form').addEventListener('submit', function(even
         type: type,
         amount: amount,
         when: getCurrentDateTime()
-    })
+    });
 
-    let dataWrapper = document.querySelector('.payments-wrapper')
-    let balanceWrapper = document.querySelector('.balance-wrapper')
+    let dataWrapper = document.querySelector('.payments-wrapper');
+    let balanceWrapper = document.querySelector('.balance-wrapper');
 
-    dataWrapper.innerHTML = ''
-    balanceWrapper.innerHTML = ''
+    dataWrapper.innerHTML = '';
+    balanceWrapper.innerHTML = '';
 
-    load_data('.payments-wrapper', payments, '.balance-wrapper')
+    load_data('.payments-wrapper', payments, '.balance-wrapper');
 
-    document.getElementById('amount').value = ''
-})
+    document.getElementById('amount').value = '';
+});
 
 function getCurrentDateTime() {
-    const now = new Date()
-    const year = now.getFullYear()
-    let month = now.getMonth() + 1
-    let day = now.getDate()
-    let hours = now.getHours()
-    let minutes = now.getMinutes()
-    let seconds = now.getSeconds()
+    const now = new Date();
+    const year = now.getFullYear();
+    let month = now.getMonth() + 1;
+    let day = now.getDate();
+    let hours = now.getHours();
+    let minutes = now.getMinutes();
+    let seconds = now.getSeconds();
 
-    month = month < 10 ? `0${month}` : month
-    day = day < 10 ? `0${day}` : day
-    hours = hours < 10 ? `0${hours}` : hours
-    minutes = minutes < 10 ? `0${minutes}` : minutes
-    seconds = seconds < 10 ? `0${seconds}` : seconds
+    month = month < 10 ? `0${month}` : month;
+    day = day < 10 ? `0${day}` : day;
+    hours = hours < 10 ? `0${hours}` : hours;
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    seconds = seconds < 10 ? `0${seconds}` : seconds;
 
-    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+    return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
-document.getElementById("salary").addEventListener('keyup', function(event) {
+document.getElementById("salary").addEventListener('keyup', function (event) {
     let inputValue = event.target.valueAsNumber;
-    console.log(inputValue);
 
     if (inputValue < 0) {
         event.target.classList.add('error');
@@ -124,15 +119,13 @@ document.getElementById("salary").addEventListener('keyup', function(event) {
         event.target.nextElementSibling.classList.remove('show');
     }
 });
- 
+
 document.querySelector("#payment-form").addEventListener('reset', function () {
     resetToDefaultValues();
 });
 
 function resetToDefaultValues() {
-
     const balance_wrapper = document.querySelector('.balance-wrapper');
-    
     const defaultMonthlySalary = 0;
     const defaultIncome = 0;
     const defaultExpense = 0;
@@ -156,4 +149,3 @@ function resetToDefaultValues() {
     let resetButton = document.querySelector('.reset');
     resetButton.classList.remove('show');
 }
-
